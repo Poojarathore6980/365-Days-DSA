758cd7aaa600aedf7dc12009cc60589731591c98
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2026-04-22 22:22:20 +0530

    Leetciode 234 Palindrome Linked List

---
+++ 234_palindromLinkedList/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    static ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null||head.next==null) return true;
+        
+        // finding middle by fast and slow pointer
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+        ListNode secondHalf=reverse(slow);
+        ListNode temp1=head;
+        ListNode temp2=secondHalf;
+        while(temp2!=null){
+            if(temp1.val!=temp2.val) return false;
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        return true;
+        
+    }
+}

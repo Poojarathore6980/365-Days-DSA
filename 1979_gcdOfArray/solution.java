bc1ea1129c791f7c352f1bd8e49f2cb5a9efc462
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2026-06-29 15:19:22 +0530

    Leetcode 1979 GCD of array

---
+++ 1979_gcdOfArray/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findGCD(int[] nums) {
+
+        int min = nums[0];
+        int max = nums[0];
+
+        // Find minimum and maximum
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        int gcd = 1;
+
+        // Check all possible divisors
+        for (int i = 1; i <= min; i++) {
+            if (min % i == 0 && max % i == 0) {
+                gcd = i;
+            }
+        }
+
+        return gcd;
+    }
+}

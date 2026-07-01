9542224946f20ff8107662bca6e3713a31d88dff
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2026-07-01 20:38:08 +0530

    Leetcode 392 In Subsequences usin 2 pointer approach

---
+++ 392_isSubsequence/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()){
+            return false;
+        }
+        int i =0;
+        for(int j = 0 ; j<t.length();j++){
+           if(i<s.length() && s.charAt(i) == t.charAt(j)){
+            i++;
+           }
+            
+            
+
+        }
+        return i==s.length();
+
+
+
+
+    }
+}

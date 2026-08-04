4803bceddd43bb4c5bdff83dcd7fa39c84253a30
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2026-08-04 10:02:07 +0530

    leetcode 3731

---
+++ 3731_findMissingElement/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        for(int num : nums){
+            if(num<min){
+                min=num;
+            }
+            if(num>max){
+                max=num;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i = min;i<=max;i++){
+            boolean found= false;
+            for (int num : nums) {
+                if (num == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                ans.add(i);
+        }
+        return ans;
+    }
+}

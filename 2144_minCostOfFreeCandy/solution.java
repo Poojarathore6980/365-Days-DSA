b8dfd43a943d9b25138456a6cb461ca992a7c158
Pooja Kanwar <pooja.kanwar.rathore6980@gmail.com>
2026-08-21 22:37:05 +0530

    Leetcode 2144

---
+++ 2144_minCostOfFreeCandy/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+       int count=0;
+       int ans=0;
+       Arrays.sort(cost);
+       for(int i =cost.length-1;i>=0;i--){
+        count++;
+        if(count%3==0){
+            continue;
+        }
+        else{
+          ans+=cost[i];
+        }
+       }
+       return ans;
+    }
+}
